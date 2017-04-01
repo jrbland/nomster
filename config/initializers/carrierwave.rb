ea39d509719954e55e5cf8c401e0b117281690de
Justin Bland <jrbland@yahoo.com>
2017-04-01 19:53:02 +0000

    updated email, debugged

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
+  #config.storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
+  
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
