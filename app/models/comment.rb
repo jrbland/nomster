b480e9cb0cd1d3cd67fce5e5409186a2c6eab869
Justin Bland <jrbland@yahoo.com>
2017-04-11 04:29:48 +0000

    configured sendgrid

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
-
+  after_create :send_comment_email
+  
   RATINGS = {
     'one star': '1_star',
     'two stars': '2_stars',
@@ -9,8 +10,12 @@ class Comment < ApplicationRecord
     'four stars': '4_stars',
     'five stars': '5_stars'
   }
-
+  
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+  
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
   end
 end
