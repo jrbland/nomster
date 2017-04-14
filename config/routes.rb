ee443e7e1414bf4b8174acb7ee328491e15db431
Justin Bland <jrbland@yahoo.com>
2017-04-14 19:53:39 +0000

    added base my profile page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :photos, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
 end
